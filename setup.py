da58b628532065734988c270d50304dbc5a31494
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-08-05 15:39:49 +0800

    add

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(name='notedrive',
       author='niuliangtao',
       author_email='1007530194@qq.com',
       url='https://github.com/1007530194',
-
       packages=find_packages(),
       install_requires=install_requires,
       )
