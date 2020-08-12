f8f0de9c3d5d58c3f73845b9fa800278d275eecf
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-08-12 11:53:44 +0800

    add

--- example/lanzou_example.py
+++ example/lanzou_example.py
@@ -28,21 +28,33 @@ def example4():
     # downer.upload_file('/Users/liangtaoniu/workspace/MyDiary/tmp/models/yolo/configs/yolov3.h5', folder_id=2129808)
     # downer.upload_file('/Users/liangtaoniu/workspace/MyDiary/tmp/models/yolo/configs/yolov3.weights', folder_id=2129808)
     # downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/yolov4.weights', folder_id=2129808)
-    res = downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/annotations_trainval2017.zip',
-                             folder_id=2160967)
-    res = downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/val2017.zip', folder_id=2160967)
+    # res = downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/annotations_trainval2017.zip',
+    #                         folder_id=2160967)
+    # res = downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/val2017.zip', folder_id=2160967)
+
+    # res = downer.upload_file('/Users/liangtaoniu/tmp/dataset/movielens/ml-100k.zip', folder_id=2184164)
+    # res = downer.upload_file('/Users/liangtaoniu/tmp/dataset/movielens/ml-1m.zip', folder_id=2184164)
+    # res = downer.upload_file('/Users/liangtaoniu/tmp/dataset/movielens/ml-10m.zip', folder_id=2184164)
+    # res = downer.upload_file('/Users/liangtaoniu/tmp/dataset/movielens/ml-20m.zip', folder_id=2184164)
+
+    # res = downer.upload_file('/Users/liangtaoniu/workspace/dataset/models/ml-25m.zip', folder_id=2184164)
+
+    res = downer.upload_file(
+        '/Users/liangtaoniu/workspace/MyDiary/notechats/notekeras/example/yolo4/models/yolov4-416.h5',
+        folder_id=2129808)
 
     print(res)
     pass
 
 
 def example5():
-    print(downer.get_dir_list(folder_id=2160967))
-    
+    print(downer.get_dir_list(folder_id=2184164))
+
 
 # example1()
+# example2()
 # example3()
 example4()
-# example2()
 example5()
 # https://wws.lanzous.com/b01hjn3aj
+# print(downer._session.cookies)
